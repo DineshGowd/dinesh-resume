88802bbfb2550913b37c45152f641bb86fc363c4
Patel Dinesh Gowd <pateldineshgowd@gmail.com>
2023-01-13 18:17:18 +0530

    Updating iframe

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -7,6 +7,7 @@ const Resume = () => {
     <div>
         <ResumeLayout />
        {true && <Download /> }
+       <iframe title='dinesh-live' width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=UCtilkqDskFXxH6tA8DKCsOA" frameborder="0" allowfullscreen></iframe>
     </div>
   )
 }
